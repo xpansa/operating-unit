ee30690637d1c6d4d0619d087b1c4629d6e8e28b
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2016-02-02 15:08:02 +0530

    [ADD] : Added account_entries_analysis_ou module by extending functionality.

---
+++ account_entries_analysis_operating_unit/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Accounting Entries Analysis Operating Unit",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "author": "Eficent, Serpent Consulting Services Pvt. Ltd., "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["account_entries_report_hooks"],
+    "license": "AGPL-3",
+    "data": [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
