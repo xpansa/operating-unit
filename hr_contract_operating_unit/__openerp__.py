ec1950a03e2bbee2f134d881ffc46ec989896629
SerpentCS <support@serpentcs.com>
2016-08-29 18:04:52 +0530

    [ADD] added Readme file and minor fixes

--- hr_contract_operating_unit/__openerp__.py
+++ hr_contract_operating_unit/__openerp__.py
@@ -1,37 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#              Jordi Ballester Alomar <jordi.ballester@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "HR Contract Operating Unit",
     "version": "8.0.1.0.0",
     "license": 'AGPL-3',
-    "author": "Eficent",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
     "category": "Generic Modules/Human Resources",
     "depends": ["hr_contract", "operating_unit"],
-    "description": """
-HR Contract Operating Unit
-=========================
-Adds a the operating unit to the HR Contract.
-    """,
     "data": [
         "views/hr_contract_view.xml",
         "security/hr_contract_security.xml"
