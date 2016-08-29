ac92a69bfa2f8ae5ad66e3b5f0d1ac8e9bb2c509
SerpentCS <support@serpentcs.com>
2016-08-29 18:13:40 +0530

    [ADD] added Readme file and minor fixes

--- crm_claim_operating_unit/__openerp__.py
+++ crm_claim_operating_unit/__openerp__.py
@@ -2,27 +2,21 @@
 # © 2015 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in CRM Claims",
     "version": "9.0.1.0.0",
-    "author": "Eficent",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
     "website": "http://www.eficent.com",
-    "category": "CRM",
+    "category": "Sales",
     "depends": ["crm_claim", "operating_unit"],
-    "description": """
-Operating Unit in CRM Claims
-============================
-This module introduces the security rules of operating unit to CRM Claims.
-A user can only view and manage the claims associated to the warehouse of
-the operating units that he has access to.
-
-    """,
     "data": [
         "security/crm_security.xml",
         "views/crm_claim_view.xml"
     ],
     'installable': True,
-    'active': False,
 }
