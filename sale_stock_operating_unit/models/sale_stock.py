1def2e58161ec4fd881345a2ef51686f94414cd3
ahenriquez <ahenriquez@eficent.com>
2016-09-12 19:11:20 +0200

    [IMP] add a constraint

--- sale_stock_operating_unit/models/sale_stock.py
+++ sale_stock_operating_unit/models/sale_stock.py
@@ -4,7 +4,7 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from openerp import api, models
-
+from openerp.exceptions import ValidationError
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
@@ -22,3 +22,18 @@ class StockMove(models.Model):
                 sale_line.order_id.operating_unit_id.id
             })
         return values
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    @api.constrains('operating_unit_id', 'warehouse_id')
+    def _check_wh_operating_unit(self):
+        for rec in self:
+            if rec.operating_unit_id and\
+                    rec.operating_unit_id != \
+                            rec.warehouse_id.operating_unit_id:
+                raise ValidationError(_('Configuration error!\nThe Operating'
+                                        'Unit in the Sales Order and in the'
+                                        ' Warehouse must be the same.'))
