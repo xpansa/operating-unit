87f194e24f78fd7fc04c69061a088ce42ca0e88d
ahenriquez <ahenriquez@eficent.com>
2016-09-13 13:36:09 +0200

    [FIX] depends on account OU. Removed deprecated key.  Revert test to previous commit.

--- sale_operating_unit/__openerp__.py
+++ sale_operating_unit/__openerp__.py
@@ -14,11 +14,10 @@
     "license": "LGPL-3",
     "website": "http://www.eficent.com",
     "category": "Sales Management",
-    "depends": ["sale", "operating_unit"],
+    "depends": ["sale", "operating_unit", "account_operating_unit"],
     "data": [
         "views/sale_view.xml",
         "security/sale_security.xml",
     ],
-    'installable': True,
-    'active': False,
+    'installable': True
 }
--- sale_operating_unit/tests/test_sale_operating_unit.py
+++ sale_operating_unit/tests/test_sale_operating_unit.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openerp import netsvc
 from openerp.tests import common
 
@@ -17,6 +17,7 @@ class TestSaleOperatingUnit(common.TransactionCase):
         self.sale_model = self.env['sale.order']
         self.sale_order_model = self.env['sale.order.line']
         self.acc_move_model = self.env['account.move']
+        self.acc_invoice_model = self.env['account.invoice']
         self.res_company_model = self.env['res.company']
         self.product_model = self.env['product.product']
         self.operating_unit_model = self.env['operating.unit']
@@ -24,14 +25,14 @@ class TestSaleOperatingUnit(common.TransactionCase):
         self.payment_model = self.env['sale.advance.payment.inv']
         # Company
         self.company = self.env.ref('base.main_company')
-        self.grp_sale_manager = self.env.ref('base.group_sale_manager')
+        self.grp_sale_user = self.env.ref('base.group_sale_manager')
         self.grp_acc_user = self.env.ref('account.group_account_invoice')
+        # Main Operating Unit
+        self.ou1 = self.env.ref('operating_unit.main_operating_unit')
         # B2B Operating Unit
         self.b2b = self.env.ref('operating_unit.b2b_operating_unit')
         # B2C Operating Unit
         self.b2c = self.env.ref('operating_unit.b2c_operating_unit')
-        # Main Operating Unit
-        self.ou1 = self.env.ref('operating_unit.main_operating_unit')
         # Payment Term
         self.pay = self.env.ref('account.account_payment_term_immediate')
         # Customer
@@ -41,29 +42,25 @@ class TestSaleOperatingUnit(common.TransactionCase):
         # Partner
         self.partner1 = self.env.ref('base.res_partner_1')
         # Products
-        self.product1 = self.env.ref('product.product_product_7')
-        self.product2 = self._create_product(self.product1)
+        self.product1 = self.env.ref\
+            ('product.product_product_7')
+        self.product1.write({'invoice_policy': 'order'})
         # Create user1
-        self.user1 = self._create_user('user_1', [self.grp_sale_manager,
+        self.user1 = self._create_user('user_1', [self.grp_sale_user,
                                                   self.grp_acc_user],
                                        self.company, [self.ou1, self.b2c])
         # Create user2
-        self.user2 = self._create_user('user_2', [self.grp_sale_manager,
+        self.user2 = self._create_user('user_2', [self.grp_sale_user,
                                                   self.grp_acc_user],
                                        self.company, [self.b2c])
         # Create Sale Order1
         self.sale1 = self._create_sale_order(self.user1.id, self.customer,
-                                             self.product2, self.pricelist,
+                                             self.product1, self.pricelist,
                                              self.ou1)
         # Create Sale Order2
         self.sale2 = self._create_sale_order(self.user2.id, self.customer,
-                                             self.product2, self.pricelist,
+                                             self.product1, self.pricelist,
                                              self.b2c)
-
-    def _create_product(self, product):
-        product2 = product.copy()
-        product2.write({'invoice_policy': 'order'})
-        return product2
 
     def _create_user(self, login, groups, company, operating_units,
                      context=None):
@@ -84,19 +81,34 @@ class TestSaleOperatingUnit(common.TransactionCase):
     def _create_sale_order(self, uid, customer, product, pricelist,
                            operating_unit):
         """Create a sale order."""
-        sale = self.sale_model.create({
+        sale = self.sale_model.sudo(uid).create({
             'partner_id': customer.id,
             'partner_invoice_id': customer.id,
             'partner_shipping_id': customer.id,
             'pricelist_id': pricelist.id,
-            'operating_unit_id': operating_unit.id,
+            'operating_unit_id': operating_unit.id
         })
-        self.sale_order_model.create({
+        self.sale_order_model.sudo(uid).create({
             'order_id': sale.id,
             'product_id': product.id,
             'name': 'Sale Order Line'
         })
         return sale
+
+    def _confirm_sale(self, sale):
+        sale.action_confirm()
+        payment = self.payment_model.create({
+            'advance_payment_method': 'all'
+        })
+        sale_context = {
+            'active_id': sale.id,
+            'active_ids': sale.ids,
+            'active_model': 'sale.order',
+            'open_invoices': True,
+        }
+        res = payment.with_context(sale_context).create_invoices()
+        invoice_id = res['res_id']
+        return invoice_id
 
     def test_security(self):
         """Test Sale Operating Unit"""
@@ -108,6 +120,28 @@ class TestSaleOperatingUnit(common.TransactionCase):
                                            self.ou1.id)])
         self.assertEqual(sale.ids, [], 'User 2 should not have access to '
                                        'OU %s' % self.ou1.name)
+        # Confirm Sale1
+        self._confirm_sale(self.sale1)
+        # Confirm Sale2
+        b2c_invoice_id = self._confirm_sale(self.sale2)
+        # Checks that invoice has OU b2c
+        b2c = self.acc_invoice_model.sudo(self.user2.id).search(
+                                         [('id', '=', b2c_invoice_id),
+                                          ('operating_unit_id', '=',
+                                           self.b2c.id)])
+        self.assertNotEqual(b2c.ids, [], 'Invoice should have b2c OU')
+
+
+    def test_security_2(self):
+        """Test Sale Operating Unit"""
+        # User 2 is only assigned to Operating Unit B2C, and cannot
+        # Access Sales order from Main Operating Unit.
+        sale = self.sale_model.sudo(self.user2.id).search(
+            [('id', '=', self.sale1.id),
+             ('operating_unit_id', '=',
+              self.ou1.id)])
+        self.assertEqual(sale.ids, [], 'User 2 should not have access to '
+                                       'OU %s' % self.ou1.name)
 
         sale = self.sale_model.sudo(self.user2.id).search(
             [('id', '=', self.sale2.id),
@@ -115,4 +149,4 @@ class TestSaleOperatingUnit(common.TransactionCase):
               self.b2c.id)])
 
         self.assertEqual(len(sale.ids), 1, 'User 1 should have access to '
-                                    'OU %s' % self.b2c.name)
+                                           'OU %s' % self.b2c.name)
