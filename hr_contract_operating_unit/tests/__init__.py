473ba44ba323ba0906309ed981ccfffda4d88170
SerpentCS <support@serpentcs.com>
2016-09-13 16:11:30 +0530

    [ADD] Added Test Cases.

--- hr_contract_operating_unit/tests/__init__.py
+++ hr_contract_operating_unit/tests/__init__.py
@@ -3,5 +3,4 @@
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
-from . import tests
+from . import test_hr_contract_operating_unit
