f699b76f459bc949049cd8887b41b48746193ec5
ahenriquez <ahenriquez@eficent.com>
2016-09-15 13:01:52 +0200

    [IMP] added domain to see only OUs form that company instead of using a constraint.

--- crm_claim_operating_unit/models/crm_claim.py
+++ crm_claim_operating_unit/models/crm_claim.py
@@ -17,13 +17,3 @@ class CRMClaim(models.Model):
                                         default=lambda self:
                                         self.env['res.users'].
                                         operating_unit_default_get(self._uid))
-
-    @api.multi
-    @api.constrains('operating_unit_id', 'company_id')
-    def _check_company_operating_unit(self):
-        for rec in self:
-            if rec.company_id and \
-                    rec.operating_unit_id and \
-                    rec.company_id != rec.operating_unit_id.company_id:
-                raise UserError(_('The Company in the Claim and in the \
-                 Operating Unit must be the same.'))
